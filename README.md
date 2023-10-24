0714d136d825ecbc6d9a3993286ecefc06b6ec5a
dnddigest <85869787+dnddigest@users.noreply.github.com>
2023-10-23 18:17:12 -0600

    final version of js, readme, and adding mockup

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
-# password-generator
-Refactored code to create an application that generates random passwords.
+# Password Generator
+
+## Description
+
+A password generator built to allow users to randomly generate a password.
+
+Important aspects of the generator include:
+
+- User can specify length of password.
+- User can specify what types of characters to include in the password.
+- Generated passwords will be displayed to the user.
+
+## Page Link
+
+<!-- add link -->
+
+## Mock up
+
+![The password generator includes a title, box where the generated password will be displayed, and a button to begin password generation](./assets/03-javascript-homework-demo.png)
+
+
+## License
+
+MIT License
+
+Copyright (c) [2023] [Andre Endique]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- script.js
+++ script.js
@@ -3,25 +3,34 @@ var generateBtn = document.querySelector("#generate");
 
 function generatePassword() {
 
-  // Step 1: Get Input -> asking question, get answers
-  // function passwordPrompt() 
-  //   {
-    var passLength = prompt("Password length between 8 and 150 characters:");
-    if (8 > passLength) {
-      prompt("That's not enough characters! Please enter a password length between 8 and 150 characters:")
-    } else if (passLength > 150) {
-      prompt("That's too many characters! Please enter a password length between 8 and 150 characters:")
+  // prompt to get password length from user
+  var userInput = prompt("Password length between 8 and 128 characters:")
+  var passLength = Number(userInput) 
+  console.log(passLength)
+  while (8 > passLength || passLength > 128 || passLength == NaN) {
+    if (8 > passLength && passLength == NaN) {
+      passLength = prompt("Invalid input! Please enter a password length between 8 and 128 characters:")
+    } else if (passLength > 128 && passLength == NaN) {
+      passLength = prompt("Invalid input! Please enter a password length between 8 and 128 characters:")
     }
-  // }
-    // if (8 < passLength && passLength < 150) { 
-  var includeLower = confirm("Include lowercase letters?");
-  var includeUpper = confirm("Include uppercase letters?");
-  var includeNumber = confirm("Include numbers?");
-  var includeSpecial = confirm("Include special characters?");
-  //  } else {passwordPrompt}
+  }
+  
+  // prompts for types of characters to include in password
+  var includeLower = confirm("Include lowercase letters?")
+  var includeUpper = confirm("Include uppercase letters?")
+  var includeNumber = confirm("Include numbers?")
+  var includeSpecial = confirm("Include special characters?")
 
+  // loop to ensure user selects at least one type of character
+  while (!includeLower && !includeUpper && !includeNumber && !includeSpecial) {
+    alert("Must select at least one character criteria! Try again!")
+    includeLower = confirm("Include lowercase letters?")
+    includeUpper = confirm("Include uppercase letters?")
+    includeNumber = confirm("Include numbers?")
+    includeSpecial = confirm("Include special characters?")
+  }
 
-  // Step 2: Analyze the answers -> depending on their answers, do the necessary preparation
+  // container where all possible characters will be randomly chosen from 
   var container = "";
 
   if(includeLower) {
@@ -40,8 +49,8 @@ function generatePassword() {
     container = container + "!@#$%^&*()_+-=`~;:'[]{},.<>/?"
   }
 
-
-  // Step 3: Generate the output -> After preparation, generate the password
+  
+  // randomly selecting characters from user's selected character types
   var password = ""
 
   for(i = 0; i < passLength; i++) {
@@ -51,17 +60,15 @@ function generatePassword() {
     password = password + randomChar
   }
 
-
   return password;
 }
 
-// Write password to the #password input
+// display password to user
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
